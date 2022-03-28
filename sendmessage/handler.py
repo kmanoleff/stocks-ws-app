1b3b63b3289a4b5124bc134333af803899f878a9
Kyle Manoleff <kmanoleff@il2000.com>
2022-03-28 00:43:00 -0700

    reports

--- sendmessage/handler.py
+++ sendmessage/handler.py
@@ -9,7 +9,7 @@ logger = logging.getLogger()
 logger.setLevel(logging.INFO)
 
 from aws_lambda_powertools.utilities.data_classes import event_source, APIGatewayProxyEvent
-from models import WebsocketResponse
+from models import WebsocketResponse, ReportData
 
 
 @event_source(data_class=APIGatewayProxyEvent)
@@ -22,22 +22,17 @@ def lambda_handler(event, context):
     request_body = json.loads(event.body)
     # create client, return initial accepted response
     client = boto3.client('apigatewaymanagementapi', endpoint_url='https://' + domain_name + '/' + stage)
-    initial_connection = WebsocketResponse()
-    initial_connection.statusCode = 202
-    initial_connection.message = {'accepted': True}
+    initial_connection = WebsocketResponse(status_code=202, report_data=None, message='accepted')
     client.post_to_connection(ConnectionId=connection_id, Data=initial_connection.json())
-
-    websocket_response = WebsocketResponse()
     try:
         # polygon stocks api endpoint
         base_api_endpoint = 'https://api.polygon.io/v2/aggs/ticker/'
-        api_endpoint_suffix = '/range/1/day/2022-03-25/2022-03-27?apiKey=taIMgMrmnZ8SUZmdpq9_7ANRDxw3IPIx'
+        api_endpoint_suffix = '/range/1/day/2020-01-01/2020-12-31?apiKey=taIMgMrmnZ8SUZmdpq9_7ANRDxw3IPIx'
         # retrieve ticker from request, required field
         requested_ticker = request_body.get('ticker')
         # ticker required
         if requested_ticker is None or requested_ticker == '':
-            websocket_response.statusCode = 400
-            websocket_response.message = {'error': 'ticker is required'}
+            websocket_response = WebsocketResponse(status_code=400, report_data=None, message='ticker required')
             client.post_to_connection(ConnectionId=connection_id, Data=websocket_response.json())
             client.delete_connection(ConnectionId=connection_id)
             return {'statusCode': 400}
@@ -45,17 +40,57 @@ def lambda_handler(event, context):
         logger.info('Getting stocks for ticker : ' + requested_ticker)
         stocks_api_response = requests.get(url=base_api_endpoint + requested_ticker + api_endpoint_suffix)
         logger.info(json.dumps(stocks_api_response.json()))
+        # create the report
+        report_data = create_report(stocks_api_response.json())
         # return the successful results
-        websocket_response.statusCode = 200
-        websocket_response.message = stocks_api_response.json()
+        websocket_response = WebsocketResponse(status_code=200, report_data=report_data, message='success')
         # --- emulate the long-running task ---
         time.sleep(31)
         client.post_to_connection(ConnectionId=connection_id, Data=websocket_response.json())
         client.delete_connection(ConnectionId=connection_id)
         return {"statusCode": 200}
     except Exception as e:
-        websocket_response.statusCode = 500
-        websocket_response.message = {'error': str(e)}
+        websocket_response = WebsocketResponse(status_code=500, report_data=None, message=str(e))
         client.post_to_connection(ConnectionId=connection_id, Data=websocket_response.json())
         client.delete_connection(ConnectionId=connection_id)
         return {"statusCode": 500}
+
+
+def create_report(stocks_response):
+    min_stock = None
+    max_stock = None
+    min_volume = None
+    max_volume = None
+    rolling_low = None
+    rolling_high = None
+
+    result_list = stocks_response.get('results')
+    for index, result in enumerate(result_list):
+        current_result_lowest_price = result['l']
+        current_result_highest_price = result['h']
+        current_result_volume = result['v']
+        if index == 0:
+            min_stock = current_result_lowest_price
+            max_stock = current_result_highest_price
+            min_volume = current_result_volume
+            max_volume = current_result_volume
+            rolling_low = current_result_lowest_price
+            rolling_high = current_result_highest_price
+        else:
+            if current_result_lowest_price < min_stock:
+                min_stock = current_result_lowest_price
+            if current_result_highest_price > max_stock:
+                max_stock = current_result_highest_price
+            if current_result_volume < min_volume:
+                min_volume = current_result_volume
+            if current_result_volume > max_volume:
+                max_volume = current_result_volume
+            rolling_low += current_result_lowest_price
+            rolling_high += current_result_highest_price
+    final_avg_low = rolling_low / len(result_list)
+    final_avg_high = rolling_high / len(result_list)
+    avg_stock = (final_avg_low + final_avg_high) / 2
+
+    report_data = ReportData(avg_stock=avg_stock, min_stock=min_stock, max_stock=max_stock,
+                             min_volume=min_volume, max_volume=max_volume)
+    return report_data
--- sendmessage/models.py
+++ sendmessage/models.py
@@ -2,10 +2,22 @@ import json
 
 
 class WebsocketResponse(object):
-    def __init__(self, values: dict = None):
-        values = values if values is not None else {}
-        self.statusCode: int = values.get('status_code', None)
-        self.message: object = values.get('message', None)
+    def __init__(self, status_code, report_data, message):
+        self.statusCode = status_code
+        self.reportData = report_data
+        self.message = message
+
+    def json(self):
+        return json.dumps(self, default=lambda o: o.__dict__)
+
+
+class ReportData(object):
+    def __init__(self, avg_stock, min_stock, max_stock, min_volume, max_volume):
+        self.avgStock = avg_stock
+        self.minStock = min_stock
+        self.maxStock = max_stock
+        self.minVolume = min_volume
+        self.maxVolume = max_volume
 
     def json(self):
         return json.dumps(self, default=lambda o: o.__dict__)
